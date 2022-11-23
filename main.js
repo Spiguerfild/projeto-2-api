1ac5b125200310287c5e49abf1579cfbf1e13f11
Luis Henrique <luique16@outlook.com>
2022-11-23 11:53:54 -0400

    💄 Adição da interface e estilização “

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ import '@picocss/pico'
 import './style.css'
 
 
-async function requisitarnome()
+const consultar_git = document.querySelector('#lado-esquerdo-form');
+const input_nome_git = consultar_git.nomeGit;
+const lado_dados = document.querySelector('#dadosLadoDireito');
+const btn_consultar_git = document.querySelector('#btnBuscarGit')
+
